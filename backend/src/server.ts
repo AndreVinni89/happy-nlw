051eea6e66a787d016ecdbc87e19712b1c1eae59
AndreVinni89 <andrevinnicius89@gmail.com>
2020-10-14 11:26:10 -0300

    dia 2 parte 1 terminado

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,9 +1,14 @@
 import express from 'express'
 
+import './database/connection'
+
 const app = express()
+
+app.use(express.json())
 
 app.listen(3333)
 
-app.get('/users', ()=>{ 
-    console.log("Ola")
+app.post('/orphanages', (req, res)=>{ 
+    console.log(req.body)
+    return res.json({message:"OLA MUNDO"})
 })
